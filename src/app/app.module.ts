6aa04a185f693a3aa46bb734679a1a9f4688c2f5
Eddi <eddy-bermejo@hotmail.com>
2022-06-03 17:34:20 -0500

    actividad para entregar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CharactersComponent } from './characters/characters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CharactersComponent
   ],
   imports: [
     BrowserModule
